be665883e224e37955757c2fa7bbccf7f660e746
Edu Moreno <e.m.morenolp@gmail.com>
2023-09-28 22:33:03 -0300

    clase 22-23-24

---
+++ Clase 22-23-24/PWDC/script.js
@@ -0,0 +1,8 @@
+
+
+
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+    navBar = document.querySelector(".navbar");
+    navBar.classList.toggle("active");
+};
---
+++ Clase 22-23-24/script.js
@@ -0,0 +1,21 @@
+
+
+
+/* Primeros pasos en JavaScript */
+
+// alert("Hola este es mi Javascript");
+
+let nombre0 = "Eduardo Moreno";
+
+const nombre1 = "Moreno Eduardo";
+
+var nombre2 = "Eduardo M Moreno";
+
+console.log(nombre0);
+console.log(nombre1);
+console.log(nombre2);
+
+
+
+// console.log(texto);
+
